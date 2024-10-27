d8e06089304565e6c7228ccdb001e0c36f7abbf9
hieu <hieushinnosuke@gmail.com>
2024-10-27 22:45:41 +0800

    fixing migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,44 +17,48 @@ use App\Models\Connection;
 use App\Models\Notification;
 use App\Models\Comment;
 use App\Models\Endorsement;
+use App\Models\Profile;
+
 class DatabaseSeeder extends Seeder
 {
     public function run()
     {
         // Create Users with Relationships
-        User::factory()->count(3)->withRelationships()->create();
+        User::factory()->count(2)->withRelationships()->create();
+
+        Profile::factory()->count(2)->create();
 
         // Create Companies
-        Company::factory()->count(3)->create();
+        Company::factory()->count(2)->create();
 
         // Create Jobs with Skills
-        Job::factory()->count(3)->withSkills()->create();
+        Job::factory()->count(2)->withSkills()->create();
 
         // Create Events with Attendees
-        Event::factory()->count(3)->withAttendees()->create();
+        Event::factory()->count(2)->withAttendees()->create();
 
         // Create Groups with Members
-        Group::factory()->count(3)->withMembers()->create();
+        Group::factory()->count(2)->withMembers()->create();
 
         // Create Skills
-        Skill::factory()->count(3)->create();
+        Skill::factory()->count(2)->create();
 
         // Create Applications
-        Application::factory()->count(3)->create();
+        Application::factory()->count(2)->create();
 
         // Create Posts with Comments
-        Post::factory()->count(3)->withComments(3)->create(); // Creates 3 comments for each post
+        Post::factory()->count(2)->withComments(2)->create(); // Creates 2 comments for each post
 
         // Create Messages
-        Message::factory()->count(3)->create();
+        Message::factory()->count(2)->create();
 
         // Create Connections
-        Connection::factory()->count(3)->create();
+        Connection::factory()->count(2)->create();
 
-        Endorsement::factory()->count(3)->create();
+        Endorsement::factory()->count(2)->create();
 
         // Create Notifications
-        Notification::factory()->count(3)->create();
+        Notification::factory()->count(2)->create();
 
     }
 }
